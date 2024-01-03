aa946cc4ffbfe3e6acc9a6f74584d1832b817ad4
Esteban <tebyfall@gmail.com>
2024-01-03 19:34:50 -0300

    Login

--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,9 +1,61 @@
-function Login (){
-return(
-    <div>
-        <p>Login</p>
-    </div>
-)
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export function Login() {
+  const navigate = useNavigate();
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  function onUserInput(e: React.FormEvent<HTMLInputElement>) {
+    setUser(e.currentTarget.value);
+  }
+
+  function onPasswordInput(e: React.FormEvent<HTMLInputElement>) {
+    setPassword(e.currentTarget.value);
+  }
+
+  function onConfirmPasswordInput(e: React.FormEvent<HTMLInputElement>) {
+    setConfirmPassword(e.currentTarget.value);
+  }
+
+  function handleLogin() {
+    if (password === confirmPassword) {
+      navigate("/dashboard");
+    } else {
+      alert("Las contraseñas no coinciden. Por favor, inténtalo de nuevo.");
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <p>User Name:</p>
+        <input
+          type="text"
+          value={user}
+          onInput={onUserInput}
+        />
+      </div>
+      <div>
+        <p>Password:</p>
+        <input
+          type="password"
+          value={password}
+          onInput={onPasswordInput}
+        />
+      </div>
+      <div>
+        <p>Confirm Password:</p>
+        <input
+          type="password"
+          value={confirmPassword}
+          onInput={onConfirmPasswordInput}
+        />
+      </div>
+      <button onClick={handleLogin}>Login</button>
+    </>
+  );
 }
 
 export default Login;
